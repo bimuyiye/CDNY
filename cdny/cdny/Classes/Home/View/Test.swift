5d209411e858350aaaea3ae7db556945e3c2020e
MAC_HANMO <MAC@mac-hanmo.local>
2017-07-31 11:36:24 +0800

    测试

---
+++ cdny/cdny/Classes/Home/View/Test.swift
@@ -0,0 +1,22 @@
+//
+//  Test.swift
+//  cdny
+//
+//  Created by MAC_HANMO on 2017/7/31.
+//  Copyright © 2017年 moon. All rights reserved.
+//
+
+import UIKit
+
+class Test: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
